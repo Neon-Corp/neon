f38592ac9f196f0209f68e25a2b91d5f8c1d2ea0
Noberto Pessoa <no.neto@live.com>
2018-11-24 17:24:18 -0300

    Add login page
    Code to process the login submission is still missing.
    Also, update the navbar in other pages and include updated bootstrap assets source.

--- src/main/java/com/neon/controller/HomeController.java
+++ src/main/java/com/neon/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.neon.controller;
 
 import com.neon.model.Brand;
+import com.neon.model.User;
 import com.neon.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -20,9 +23,20 @@ public class HomeController {
         return "index";
     }
 
+    @GetMapping("/login")
+    public String loginIndex(@ModelAttribute User user) {
+        return "login/index";
+    }
+
+    @PostMapping("/login")
+    public String loginSubmit(@Valid @ModelAttribute User user) {
+//        TODO
+        return "redirect:/";
+    }
+
     @PostMapping("/search")
     public String searchModel(@RequestParam("deviceBrandInput") String deviceBrandId, @RequestParam("deviceModelSearchInput") String deviceModel) {
-        if (deviceModel.equals("")){
+        if (deviceModel.equals("")) {
             return "redirect:listings/search?brand=" + deviceBrandId.toString();
         } else {
             return "redirect:listings/search?brand=" + deviceBrandId.toString() + "&model=" + deviceModel;
